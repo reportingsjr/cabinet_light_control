c67812e02fa09749ac13f16e5163ab8630b76ce4
Jon Neal <reportingsjr@gmail.com>
2017-06-20 15:29:03 -0400

    Create cabint light control based off of the aquarium lights.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -28,79 +28,311 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+extern "C" {
+#include "pwm.h"
+}
+
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
+// For MQTT
+#include <PubSubClient.h>
+
+// For OTA updates
 #include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
-#include "Time.h"
-#include "Timezone.h"
+#include "wifiPassword.h"
 
-#include "updateTime.h"
-#include "localWebServer.h"
+const PROGMEM char*     MQTT_CLIENT_ID    = "cabinet_light";
+const PROGMEM char*     MQTT_SERVER_IP    = "192.168.0.22";
+const PROGMEM uint16_t  MQTT_SERVER_PORT  = 1883;
 
-TimeChangeRule usEDT = {"EDT", Second, Sun, Mar, 2, -240};  //UTC - 4 hours
-TimeChangeRule usEST = {"EST", First, Sun, Nov, 2, -300};   //UTC - 5 hours
-Timezone usEastern(usEDT, usEST);
+// MQTT: topics
+// state
+const PROGMEM char*     MQTT_LIGHT_STATE_TOPIC                = "kitchen/cabinetlight";
+const PROGMEM char*     MQTT_LIGHT_COMMAND_TOPIC              = "kitchen/cabinetlight/switch";
 
-int brightness = 0;
-bool brighter = true;
+// brightness
+const PROGMEM char*     MQTT_LIGHT_BRIGHTNESS_STATE_TOPIC     = "kitchen/cabinetlight/brightness";
+const PROGMEM char*     MQTT_LIGHT_BRIGHTNESS_COMMAND_TOPIC   = "kitchen/cabinetlight/brightness/set";
 
-int pwmIntervals = 180; // three hour fade on and off
-float R = (pwmIntervals * log10(2))/(log10(1023));
+// payloads by default (on/off)
+const PROGMEM char*     LIGHT_ON          = "ON";
+const PROGMEM char*     LIGHT_OFF         = "OFF";
 
-// variable here temporarily to simulate faster days
-int timeWarp = 0;
+// state of the sun
+const PROGMEM char*    MQTT_SUN_TOPIC = "sun";
+
+// sun payloads
+const PROGMEM char*    SUNSET = "sunset";
+const PROGMEM char*    SUNRISE = "sunrise";
+
+boolean m_light_state = false;
+int m_light_brightness = 0;
+boolean m_sun_up = false;
+
+// pin used for the led (PWM)
+const PROGMEM uint8_t LIGHT_PIN = D1;
+
+const uint8_t MSG_BUFFER_SIZE = 20;
+char m_msg_buffer[MSG_BUFFER_SIZE]; 
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+time_t PIR_time_passed = 0;
+boolean on_from_PIR = false;
+
+// number of milliseconds to wait to turn off LEDs once PIR no longer detects someone
+const unsigned long turn_off_countdown = 120000;
+
+// state machine:
+// on_pir = PIR sensor detects presence
+// counting_down_pir = PIR sensor doesn't detect anyone, but on
+// off_pir = PIR sensor hasn't detected anyone in X seconds
+// off_ha = turned off in home assistant
+// on_ha = turned off in home assistant
+enum current_state_enum {
+  on_pir,
+  counting_down_pir,
+  off_pir,
+  off_ha,
+  on_ha
+};
+
+current_state_enum current_state = off_pir;
+
+// function called to adapt the brightness and the state of the led
+void setLightState() {
+  if (m_light_state) {
+    //analogWrite(LIGHT_PIN, m_light_brightness);
+    pwm_set_duty(m_light_brightness, 0);
+    pwm_start();
+    Serial.print("INFO: Brightness: ");
+    Serial.println(m_light_brightness);
+  } else {
+    //analogWrite(LIGHT_PIN, 0);
+    pwm_set_duty(0, 0);
+    pwm_start();
+    Serial.println("INFO: Turn light off");
+  }
+}
+
+// function called to publish the state of the led (on/off)
+void publishLightState() {
+  if (m_light_state) {
+    client.publish(MQTT_LIGHT_STATE_TOPIC, LIGHT_ON, true);
+  } else {
+    client.publish(MQTT_LIGHT_STATE_TOPIC, LIGHT_OFF, true);
+  }
+}
+
+// function called to publish the brightness of the led
+void publishLightBrightness() {
+  snprintf(m_msg_buffer, MSG_BUFFER_SIZE, "%d", map(m_light_brightness, 0, 5000, 0, 255));
+  client.publish(MQTT_LIGHT_BRIGHTNESS_STATE_TOPIC, m_msg_buffer, true);
+}
+
+
+// function called when a MQTT message arrived
+void callback(char* p_topic, byte* p_payload, unsigned int p_length) {
+  // concat the payload into a string
+  String payload;
+  for (uint8_t i = 0; i < p_length; i++) {
+    payload.concat((char)p_payload[i]);
+  }
+  // handle message topic
+  if (String(MQTT_LIGHT_COMMAND_TOPIC).equals(p_topic)) {
+    on_from_PIR = false;
+    // test if the payload is equal to "ON" or "OFF"
+    if (payload.equals(String(LIGHT_ON))) {
+      current_state = on_ha;
+      if (m_light_state != true) {
+        m_light_state = true;
+        setLightState();
+        publishLightState();
+      }
+    } else if (payload.equals(String(LIGHT_OFF))) {
+      current_state = off_ha;
+      if (m_light_state != false) {
+        m_light_state = false;
+        m_light_brightness = 0;
+        setLightState();
+      }
+        publishLightState();
+    }
+  } else if (String(MQTT_LIGHT_BRIGHTNESS_COMMAND_TOPIC).equals(p_topic)) {
+    current_state = on_ha;
+    uint8_t brightness = payload.toInt();
+    if (brightness < 0 || brightness > 255) {
+      // do nothing...
+      return;
+    } else {
+      m_light_brightness = map(brightness, 0, 255, 0, 5000);
+      setLightState();
+      publishLightBrightness();
+    }
+  } else if (String(MQTT_SUN_TOPIC).equals(p_topic)) {
+    if(payload.equals(String(SUNRISE))) {
+      m_sun_up = true;
+    } else if(payload.equals(String(SUNSET))) {
+      m_sun_up = false;
+    }
+  }
+
+}
+
+void reconnect() {
+  Serial.print("INFO: Attempting MQTT connection...");
+  // Attempt to connect
+  if (client.connect(MQTT_CLIENT_ID)) {
+    Serial.println("\nINFO: connected");
+
+    // Once connected, publish an announcement...
+    // publish the initial values
+    publishLightState();
+    publishLightBrightness();
+
+    // ... and resubscribe
+    client.subscribe(MQTT_LIGHT_COMMAND_TOPIC);
+    client.subscribe(MQTT_LIGHT_BRIGHTNESS_COMMAND_TOPIC);
+    client.subscribe(MQTT_SUN_TOPIC);
+  } else {
+    Serial.print("ERROR: failed, rc=");
+    Serial.print(client.state());
+  }
+}
 
 void setup ( void ) {
-  startWiFi();
-  startNTP();
-  analogWriteFreq(500);
-  analogWrite(D1, brightness);
+  Serial.begin(115200);
+  pinMode(5, OUTPUT);
+  pinMode(D2, INPUT);
+
+#define PWM_CHANNELS 1
+const uint32 period = 5000; // * 200ns ^= 1kHz
+
+uint32 io_info[PWM_CHANNELS][3] = {
+  // MUX, FUNC, PIN
+  {PERIPHS_IO_MUX_GPIO5_U, FUNC_GPIO5, 5}
+};
+
+
+uint32 pwm_duty_init[PWM_CHANNELS] = {0};
+
+pwm_init(period, pwm_duty_init, PWM_CHANNELS, io_info);
+pwm_start();
+
+
+  //analogWriteFreq(500);
+  //analogWrite(D1, m_light_brightness);
+  setLightState();
+
+
+  // init the WiFi connection
+  Serial.println();
+  Serial.println();
+  Serial.print("INFO: Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("INFO: WiFi connected");
+  Serial.print("INFO: IP address: ");
+  Serial.println(WiFi.localIP());
+
+  //ArduinoOTA.setPassword((const char *)"123");
+  ArduinoOTA.setHostname("cabinetLights");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+
+  // init the MQTT connection
+  client.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
+  client.setCallback(callback);
+}
+
+
+// time from millis() when the PIR sensor no longer detected someone
+unsigned long time_turned_off = 0;
+
+void state_machine() {
+  if(current_state == off_pir) {
+    if(digitalRead(D2)) {
+      current_state = on_pir;
+      m_light_state = true;
+      m_light_brightness = 0;
+      if(m_sun_up) {
+        int end_brightness = 2000;
+        for(int i = 0; i < 10; i++) {
+          m_light_brightness += end_brightness/10;
+          setLightState();
+          publishLightState();
+          publishLightBrightness();
+          // we want a 250ms delay total
+          delay(250/10);
+        }
+      } else {
+        for(int i = 0; i < 5; i++) {
+          m_light_brightness += 1;
+          setLightState();
+          publishLightState();
+          publishLightBrightness();
+          // we want a 250ms delay total
+          delay(250/5);
+        }
+      }
+    }
+  } else if(current_state == on_pir) {
+    if(!digitalRead(D2)) {
+      current_state = counting_down_pir;
+      time_turned_off = millis();
+    }
+  } else if(current_state == counting_down_pir) {
+    if(digitalRead(D2)) {
+      current_state = on_pir;
+    } else if(millis() - time_turned_off > turn_off_countdown) {
+      current_state = off_pir;
+      m_light_state = false;
+      setLightState();
+      publishLightBrightness();
+      publishLightState();
+    }
+  } else if(current_state == on_ha) {
+    // this state overrides the PIR via MQTT so don't do anything here
+  } else if(current_state == off_ha) {
+    // this state is currently no different from off_pir, so switch straight to that
+    current_state = off_pir;
+  }
 }
 
 void loop ( void ) {
-  server.handleClient();
-  
-  // artificially increase the time by 15 mins ever loop to simulate faster days.
-  time_t utc = now();// + timeWarp*900;
-  time_t eastern = usEastern.toLocal(utc);
+  ArduinoOTA.handle();
 
-  if(hour(eastern) >= 7 && hour(eastern) < 10) {
-    Serial.println("Getting brighter!");
-    float minutesSince7 = (hour(eastern) - 7)*60 + minute(eastern);
-    brightness = pow(2, (minutesSince7 /  R)) - 1;
+  if (!client.connected()) {
+    reconnect();
   }
-  if(hour(eastern) >= 10 && hour(eastern) < 17) {
-    Serial.println("Peak brightness!");
-    brightness = 1023;
-  }
-  if(hour(eastern) >= 17 && hour(eastern) < 20) {
-    // dim LEDs
-    Serial.println("Getting dimmer!");
-    float minutesUntil8 = (20 - hour(eastern))*60 - minute(eastern);
-    brightness = pow(2, (minutesUntil8 / R)) - 1;
-  }
-  if(hour(eastern) >= 20 || hour(eastern) < 7) {
-    Serial.println("Peak darkness!");
-    brightness = 0;
-  }
-
-
-  if(lightToggleOverride == 1) {
-    brightness = 1023;
-  }
-  if(lightToggleOverride == 2) {
-    brightness = 0;
-  }
-  Serial.print("Current time:");
-  Serial.println(hour(eastern));
-  Serial.print("Current brightness:");
-  Serial.println(brightness);
-  analogWrite(D1, brightness);
-
-  delay(500);
-  // Let's do the time warp again!
-  //timeWarp++;
+  state_machine();
+  client.loop();
 }
 
